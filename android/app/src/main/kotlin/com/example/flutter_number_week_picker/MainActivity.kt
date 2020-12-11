b26db47e937011583279ad4e745c2d711c3b2fc2
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-12-11 15:05:54 +0530

    FLUT-3165 - Sample and screenshot added.

---
+++ android/app/src/main/kotlin/com/example/flutter_number_week_picker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_number_week_picker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
+
+void main() => runApp(WeekNumber());
+
+class WeekNumber extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Card(
+            margin: const EdgeInsets.fromLTRB(50, 100, 50, 100),
+            child: SfDateRangePicker(
+              monthViewSettings:
+                  DateRangePickerMonthViewSettings(numberOfWeeksInView: 2),
+            ),
+          ),
+        ),
+
+        // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+    );
+  }
+}
